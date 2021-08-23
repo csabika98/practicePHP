bd1013fadc336a342871ec35e7447b991f8db0a8
csabika98 <6ix9inegotti@naver.com>
2021-08-23 11:55:21 +0200

    practicing the basics of PHP: Variables, Arrays, Operators..etc

---
+++ associative_array.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <title>Associative Array</title>
+</head>
+<body>
+    
+    
+    
+    
+<?php
+    
+// normal array    
+$name = array('juanita', 'maria', 'jose');
+print_r($name);
+echo $name[2] . "<br>";
+    
+//assoc array    
+    
+$names = array("first_name" => 'Edwin');    
+
+print_r($names);    
+    
+    
+?>    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</body>
+</html>
